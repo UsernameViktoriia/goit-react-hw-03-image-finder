61a6c15160e752972175217df16b731df248f0a8
UsernameViktoriia <tori-boss@ukr.net>
2022-11-27 13:40:42 +0200

    закоментила зайве

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
-import { Button } from './Button/Button';
+// import { Button } from './Button/Button';
 
 export class App extends React.Component {
   state = {
